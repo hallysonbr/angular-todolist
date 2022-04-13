c8ea870970c6179f1156450de7e334b35f74423c
Hallyson Bruno <hallyson.bruno@hotmail.com>
2022-04-13 17:32:27 -0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+
+  public todos: Todo[] = []; // []
+  //public todos: any[]; // undefined
+  public title: string = 'Minhas tarefas';
+
+  constructor() {
+      this.todos.push(new Todo(1, 'Passear com o cachorro', false));  
+      this.todos.push(new Todo(2, 'Ir ao supermercado', false));  
+      this.todos.push(new Todo(3, 'Cortar o cabelo', true));  
+  }
+  
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if(index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
+  }
+
+  markAsUndone(todo: Todo) {
+    todo.done = false;
+  }
 }
