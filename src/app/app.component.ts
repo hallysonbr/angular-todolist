bcd5cf2ef64dc20ea95735e84ad1ef84e3aee85d
Hallyson Bruno <hallyson.bruno@hotmail.com>
2022-04-20 17:34:14 -0300

    Estilizando página e adicionando mais elementos.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { Todo } from 'src/models/todo.model';
 export class AppComponent {
 
   //public todos: any[]; // undefined
+  public mode = 'list';
   public todos: Todo[] = []; // []
   public titulo: string = 'Minhas Tarefas';
   public form: FormGroup;
@@ -22,9 +23,25 @@ export class AppComponent {
           Validators.required
         ])]
       })
-      this.todos.push(new Todo(1, 'Passear com o cachorro', false));  
-      this.todos.push(new Todo(2, 'Ir ao supermercado', false));  
-      this.todos.push(new Todo(3, 'Cortar o cabelo', true));  
+
+      this.load();
+
+      // this.todos.push(new Todo(1, 'Passear com o cachorro', false));  
+      // this.todos.push(new Todo(2, 'Ir ao supermercado', false));  
+      // this.todos.push(new Todo(3, 'Cortar o cabelo', true));  
+  }
+
+  add() {
+    //this.form.value => {title: 'Titulo'}
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.save();
+    this.clear();
+  }
+
+  clear(){
+    this.form.reset();
   }
   
   remove(todo: Todo) {
@@ -32,13 +49,34 @@ export class AppComponent {
     if(index !== -1) {
       this.todos.splice(index, 1);
     }
+    this.save();
   }
 
   markAsDone(todo: Todo) {
     todo.done = true;
+    this.save();
   }
 
   markAsUndone(todo: Todo) {
     todo.done = false;
+    this.save();
+  }
+
+  save() {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+
+  load() {
+    const data = localStorage.getItem('todos');
+    if(data){
+      this.todos = JSON.parse(data)
+    }else{
+      this.todos = [];
+    }    
+  }
+
+  changeMode(mode: string) {
+    this.mode = mode;  
   }
 }
